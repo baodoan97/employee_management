288a80a5e8beb894b0cb12f6e8d4fa9906e68e6b
ARYPHAMM <quoctienphamm@gmail.com>
2019-02-21 14:10:47 +0700

    edit task select images and delete images

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_task, only: [:edit, :update, :show, :destroy]
+	before_action :remove_images_file, only: [:destroy]
 	def new
 		@task = Task.new
 	end
@@ -29,7 +30,21 @@ class TasksController < ApplicationController
 	end
 
 	def update
-		if @task.update(task_params)
+        paTASK = {
+        	 "taskname" => task_params[:taskname],
+        	 "content" => task_params[:content],
+             }
+		if @task.update(paTASK)
+			if task_params[:photo] != nil
+			 i = 0
+		       while (i < task_params[:photo].count) do
+		          @image = Image.new
+				  @image.task = @task
+				  @image.pictrue = task_params[:photo][i]
+				  @image.save
+				  i = i + 1
+		       end
+		    end   	
 			flash[:success] = "Task was updated"
 			redirect_to task_path(@task)
 		else
@@ -39,6 +54,21 @@ class TasksController < ApplicationController
 
 	def show
 		
+	end
+
+	def destroyimage
+		i = 0
+		while i < params[:delete][:image_ids].count do
+			begin  
+             u = Image.find(params[:delete][:image_ids][i].to_i)
+             u.destroy
+            rescue 
+
+            end  
+             i = i + 1  
+		end
+		# @task = Task.find(params[:task][:task_id][0].to_i)
+		redirect_to edit_task_path($s)
 	end
 
 	def index
@@ -54,9 +84,15 @@ class TasksController < ApplicationController
 	def task_params
 		params.require(:task).permit(:taskname, :content,photo: [])
 	end
-
+    
 	def set_task
 		@task = Task.find(params[:id])
 	end
+
+	def remove_images_file
+    	@task.images.each do |t|
+    		 t.destroy
+	    end 
+    end
 
 end
