a54e32f52d065ac32bca4a66f4d327c5e8d92cd9
ARYPHAMM <quoctienphamm@gmail.com>
2019-02-22 16:10:39 +0700

    update index user and task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-
+    before_action :require_user
 	def new
 		@user = User.new
 	end
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 	end
 
 	def index
-
+    @users = User.all
 	end
 
 	def edit
@@ -48,5 +48,6 @@ class UsersController < ApplicationController
 	def set_user
 		@user = User.find(params[:id])
 	end
+
 
 end
