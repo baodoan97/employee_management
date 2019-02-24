0e5ce6fe6d260ae6d7392b9a6e90deb455f08081
ARYPHAMM <quoctienphamm@gmail.com>
2019-02-24 11:44:36 +0700

    update edit task may remove user

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   delete 'images', to:  'tasks#destroyimage'
+  delete 'taskusers', to:  'tasks#removetaskuser'
 end
